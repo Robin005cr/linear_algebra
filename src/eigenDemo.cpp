0b90afe61ea3d2e7d45f62f656a08285a990c75b
Robin CR <robin005cr@gmail.com>
2025-11-26 23:33:32 +0530

    Info comment and some inbuilt functions

--- src/eigenDemo.cpp
+++ src/eigenDemo.cpp
@@ -1,3 +1,16 @@
+/*
+* project   : https://github.com/Robin005cr/linear_algebra
+* file name : eigenDemo.cpp
+* author    : Robin CR
+* mail id   : robinchovallurraju@gmail.com
+* portfolio : https://robin005cr.github.io/
+*
+* Note : If any mistakes, errors, or inconsistencies are found in the code, please feel free to mail me.
+* Suggestions for improvements or better methods are always welcome and appreciated.
+* I value constructive feedback and aim to continuously improve the quality of the work.
+*
+*/
+
 #include <iostream>
 #include <Eigen/CXX11/Tensor>
 using namespace std;
@@ -11,5 +24,15 @@ int main()
                        {4.0, 5.0, 6.0}});
 
     cout << "myTensor :" <<myTensor<< endl;
+
+    Tensor<float, 2> Tensor2(2, 3);
+    Tensor2.setRandom();
+
+    cout << "Tensor2 :" << Tensor2 << endl;
+
+	int size = Tensor2.size();
+	int rows = Tensor2.dimension(0);
+	int cols = Tensor2.dimension(1);
+	cout << "Size: " << size << ", Rows: " << rows << ", Cols: " << cols << endl;
     return 0;
 }
